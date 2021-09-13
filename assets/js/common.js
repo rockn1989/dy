91507c185f481bcf36b2d97a49032d1bd21c4254
rockn1989 <gold_100@bk.ru>
2021-09-13 12:49:03 +0300

    add search form desk

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,25 @@ $(function () {
 	console.log('App start');
 
   /**
+   * Toggle desktop form search
+   */
+  const headerDesk = $('.header-desk');
+  const searchFormDesk = $('.form-search-desk-wrapper .search-form');
+  const closeForm = $('.form-search-desk-wrapper .icon-close');
+
+
+  $('.js__toggle-search-desk').on('click', function (evt) {
+    evt.preventDefault();
+    headerDesk.addClass('show-search');
+    searchFormDesk.find('input[type="text"]').focus();
+  });
+
+  closeForm.on('click', function (evt) {
+    evt.preventDefault();
+    headerDesk.removeClass('show-search');
+  });
+
+  /**
    * Mobile nav
    */
 
